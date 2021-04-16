cadcfad7ac7ea9b8f0943fb7f685de36dfafd72c
deepakbharti434 <deepakbharati16@gmail.com>
2021-04-16 14:53:03 +0530

    first

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API = axios.create ({baseURL:'http://localhost:5000'});
+const API = axios.create ({baseURL:'https://clbrasion.herokuapp.com'});
 
 API.interceptors.request.use((req) => { 
     if (localStorage.getItem('profile')) {
